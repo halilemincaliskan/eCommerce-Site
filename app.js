a1a2bab2c1251204ce624e9eb78268b15fcf80d4
Halil Emin Çalışkan <halileminclskn@gmail.com>
2023-09-20 18:10:02 +0300

    HTML Template Implemented
    -eCommercial HTML Template implemented
    -Ejs installed
    -Template files configured for ejs
    -Public file created
    -BaseURL configured

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 
 const app = express()
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs')
 
 app.listen(3000)
 
+app.locals.baseURL = "localhost:3000"
+
 app.get('/',(req,res) => {
-    res.send('<h1>Hello World</h1>')
+    res.render('index')
 })
